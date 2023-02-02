946bd47a3c4e18f5ca5dbeb91415286476a59e90
Erick <112605408+Ep1ore@users.noreply.github.com>
2023-02-02 17:52:03 -0600

    Appended home page JS

---
+++ src/home.js
@@ -0,0 +1,23 @@
+import defaultPage from './index.js';
+
+export default function createHomePage() {
+    defaultPage();
+    const homeTitle = document.createElement("h1");
+    homeTitle.id = "home-title";
+    homeTitle.textContent = "Welcome to the Epic dragon turbomax 123's restaurant";
+    const homeParagraph = document.createElement("p");
+    homeParagraph.id = "home-paragraph";
+    homeParagraph.innerHTML = `The only restaurant in which we make all types of food
+    and we <strong>LITERALLY</strong> sell it to you, not even as a joke, like,
+    for real <strong>WE</strong> make it and then sell it, they are made with
+    ingredients, you know? Kind of cool honestly, anyway if
+    you feel too sick from the food we are not really responsible,
+    those are bad vibes, and we're not up for that tbh.`;
+    const homeDragon = document.createElement("img");
+    homeDragon.id = "home-dragon";
+
+    const textContainer = document.getElementById("text-container");
+    textContainer.appendChild(homeTitle);
+    textContainer.appendChild(homeParagraph);
+    textContainer.appendChild(homeDragon);
+}
---
+++ src/index.js
@@ -0,0 +1,43 @@
+import homePage from "./home.js";
+const content = document.getElementById("content");
+
+export default function defaultPageFormat() {
+    const dragonBackgroundImage = document.createElement("img");
+    dragonBackgroundImage.id = "dragon-background-image";
+    dragonBackgroundImage.src = "../src/img/epic-dragon.jpg";
+    const nav = document.createElement("nav");
+    const navDiv = document.createElement("div");
+    const homeBtn = document.createElement("button");
+    homeBtn.id = "home-btn";
+    homeBtn.textContent = "Home";
+    const menuBtn = document.createElement("button");
+    menuBtn.id = "menu-btn";
+    menuBtn.textContent = "Menu";
+    const contactBtn = document.createElement("button");
+    contactBtn.id = "contact-btn";
+    contactBtn.textContent = "Contact";
+    const textContainer = document.createElement("div");
+    textContainer.id = "text-container";
+
+    content.appendChild(dragonBackgroundImage);
+    content.appendChild(nav);
+     nav.appendChild(navDiv);
+      navDiv.appendChild(homeBtn);
+      navDiv.appendChild(menuBtn);
+      navDiv.appendChild(contactBtn);
+    content.appendChild(textContainer);
+}
+
+function clearContent() {
+    document.getElementById("content").innerHTML = "";
+}
+
+// defaultPageFormat();
+// homePage();
+
+// const homeBtn = document.getElementById("home-btn");
+// homeBtn.addEventListener("click", function(){
+//     clearContent(); homePage();
+// });
+const menuBtn = document.getElementById("menu-btn");
+const contactBtn = document.getElementById("contact-btn");
